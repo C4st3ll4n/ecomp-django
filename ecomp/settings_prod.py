5e5d251f1c592d4b5a270765c0e7ef845fe3ee7d
Henrique Souza <henrique.souza@elostecnologia.com.br>
2020-07-14 15:11:41 -0300

    ajustes no deploy

--- ecomp/settings_prod.py
+++ ecomp/settings_prod.py
@@ -1,11 +1,7 @@
-import os
-
+from .settings import *
 import dj_database_url
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-SECRET_KEY = '6$n49vdmi8i(!3%$gg@zc5qbj4b*92q+7tz#5(u^m1dhns193p'
 
 DEBUG = False
 
