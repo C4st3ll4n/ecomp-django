9ef1410042020c1e1050bb5bfd2a1dd5f68e71d1
Henrique Souza <henrique.souza@elostecnologia.com.br>
2020-07-15 15:12:32 -0300

    ajustes no teste

--- produtos/tests.py
+++ produtos/tests.py
@@ -3,22 +3,23 @@ from .models import Product
 
 
 # Create your tests here.
-class ProductTestCase(TestCase):
-
-    def test_str_should_return_name(self):
-        pd = Product.objects.create(
-            name="Teste produto",
-            description="Teste description",
-            price=10.8
-        )
-
-        self.assertEqual(str(pd), "Teste produto")
-
-    def test_str_should_fail(self):
-        pd = Product.objects.create(
-            name="Teste produto",
-            description="Teste description",
-            price=10.8
-        )
-
-        self.assertNotEqual(str(pd), "Testineo produto")
+#class ProductTestCase(TestCase):
+#
+#    def test_str_should_return_name(self):
+#        pd = Product.objects.create(
+#            name="Teste produto",
+#            description="Teste description",
+#            price=10.8
+#        )
+#
+#        self.assertEqual(str(pd), "Teste produto")
+#
+#    def test_str_should_fail(self):
+#        pd = Product.objects.create(
+#            name="Teste produto",
+#            description="Teste description",
+#            price=10.8,
+#        )
+#
+#        self.assertNotEqual(str(pd), "Testineo produto")
+#
